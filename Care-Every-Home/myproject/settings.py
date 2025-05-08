95bc78f5834407da78307fffa090a79b65f0a680
ChadapaNgamchuen <158831933+ChadapaNgamchuen@users.noreply.github.com>
2025-05-08 18:24:51 +0700

    Change Font color,add Booking page,add equipment detail

--- Care-Every-Home/myproject/settings.py
+++ Care-Every-Home/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',  # เพิ่มแอปที่สร้างขึ้นเอง
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myproject.urls'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
